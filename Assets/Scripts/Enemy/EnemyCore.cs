ebaa4a1e98f162c7048d2f2123308f1e90ac4e77
AMAKASU <ziropaku@gmail.com>
2021-06-18 11:30:48 +0900

    no message

---
+++ Assets/Scripts/Enemy/EnemyCore.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Zenra
+{
+    namespace Police
+    {
+        public enum EnemyState
+        {
+            STAY,WEPONCHANGE,SHOT,MOVE
+        }
+        public class EnemyCore : MonoBehaviour
+        {
+
+            // Start is called before the first frame update
+            void Start()
+            {
+
+            }
+
+            // Update is called once per frame
+            void Update()
+            {
+
+            }
+        }
+
+    }
+}
---
+++ Assets/Scripts/Enemy/PoliMove.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoliMove : MonoBehaviour
+{
+    Rigidbody2D _rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void FixedUpdate()
+    {
+
+    }
+    public void Move(float speed)
+    {
+        _rb.velocity = new Vector2(speed, _rb.velocity.y);
+    }
+    public void Turn()
+    {
+
+    }
+}
