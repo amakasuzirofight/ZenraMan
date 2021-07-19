b3c9abd3c91169b94d934de4511a6caf1fe0aee0
ayaha401 <oshizawadai.1192@gmail.com>
2021-07-19 19:26:26 +0900

    update PlayerHeadExplosion update PlayerDead

--- Assets/Scripts/Player/PlayerHeadExplosion.cs
+++ Assets/Scripts/Player/PlayerHeadExplosion.cs
@@ -14,6 +14,9 @@ namespace Zenra
 
             [SerializeField,Tooltip("警官射撃event")]
             GameObject enemy;
+
+            [SerializeField, Tooltip("プレイヤーAnimator")]
+            Animator playerAnimator;
             
             private IPoliceShot _IPoliceShot;
 
@@ -21,7 +24,7 @@ namespace Zenra
             private Rigidbody2D rigidbody2D = null;
             private bool _playerDead;
             private bool once = false;
-            private float dir = 0.0f;
+            private float dir = -1.0f;
 
             void Awake() 
             {
@@ -41,8 +44,17 @@ namespace Zenra
                 _playerDead = _playerCore.isDead;
                 if(_playerDead == true && once == false)
                 {
-                    Debug.Log("HeadExplosion");
-                    HeadExplosion();
+                    // Deadステートにいるのか調べる
+                    if(playerAnimator.GetCurrentAnimatorStateInfo(0).IsName("Dead") == true)
+                    {
+                        // Deadステートのアニメーションを正規化した物のある一定に位置していたら頭が吹っ飛ぶ。
+                        // 正直Event生やしたい
+                        if(playerAnimator.GetCurrentAnimatorStateInfo(0).normalizedTime >= 0.4f)
+                        {
+                            Debug.Log("HeadExplosion");
+                            HeadExplosion();
+                        }
+                    }
                 }
             }
 
