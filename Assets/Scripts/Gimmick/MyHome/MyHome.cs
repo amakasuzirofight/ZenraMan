ccd6bb99ceb82c2d68e7095468cb8a53466fb24e
kmamonnassi <ka82573@jk2.so-net.ne.jp>
2021-07-19 12:58:51 +0900

    オオイシバインダー作成

---
+++ Assets/Scripts/Gimmick/MyHome/MyHome.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Zenra
+{
+    namespace Gimmick
+    {
+        public class MyHome
+        {
+            
+        }
+    }
+}
---
+++ Assets/Scripts/Stage/Clear/StageClearView.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+namespace Zenra
+{
+    namespace StageClear
+    {
+        public class StageClearView : MonoBehaviour
+        {
+
+        }
+    }
+}
