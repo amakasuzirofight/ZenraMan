79b3c8deeb9c9ead5f3f9e55ab23c9f30ff6b20a
ayaha401 <oshizawadai.1192@gmail.com>
2021-07-19 10:46:09 +0900

    add PlayerSetHeadTexture

---
+++ Assets/Scripts/Player/PlayerSetHeadTexture.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Zenra
+{
+    namespace Player
+    {
+        public class PlayerSetHeadTexture : MonoBehaviour
+        {
+            [SerializeField, Tooltip("顔に貼るテクスチャ")]
+            RawImage rawImage;
+
+            void Start()
+            {
+                
+            }
+
+            void Update()
+            {
+                
+            }
+        }
+    }
+}
+
