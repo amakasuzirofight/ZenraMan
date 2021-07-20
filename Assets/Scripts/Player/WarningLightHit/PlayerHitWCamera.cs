f1d6a9a9e2a7b7fd090691ddf45549f0836cfc54
AMAKASU <ziropaku@gmail.com>
2021-07-20 16:23:04 +0900

    no message

--- Assets/Scripts/Player/WarningLightHit/PlayerHitWCamera.cs
+++ Assets/Scripts/Player/WarningLightHit/PlayerHitWCamera.cs
@@ -3,16 +3,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Zenra.WarnningCamera;
+using MyUtility;
 namespace Zenra.Player
 {
     public class PlayerHitWCamera : MonoBehaviour, IWhenWarnningLevelUp
     {
         public event PlayerDiscover PlayerDiscoverEvent;
-
+        PlayerCore playerCore;
         // Start is called before the first frame update
         void Start()
         {
-
+            playerCore = Locator<PlayerCore>.GetT();
         }
 
         // Update is called once per frame
@@ -24,6 +25,7 @@ namespace Zenra.Player
         {
             if (collision.gameObject.GetComponent<IIsWarnningCamera>() != null)//ÉJÉÅÉâÇæÇ¡ÇΩÇÁ
             {
+                if (playerCore.IsHide) return;
                 PlayerDiscoverEvent();
             }
         }
